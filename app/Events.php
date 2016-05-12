de7463b2f205173a8abe4c4f983980d408d8e62d
Alex Potter <alex.potter1993@gmail.com>
2016-05-12 15:58:35 +0100

    Move initial pateint timeline rendering into Patient Model

--- app/Events.php
+++ app/Events.php
@@ -22,57 +22,6 @@ class Events
     }
 
     /**
-     * @param $id
-     * @return array
-     * @throws Exception
-     */
-    public function prepareEventDataForTemplate($id)
-    {
-        // Patient data
-        $patientData = array();
-        $counter = 0;
-
-        if (!$this->getAllEventsWithCodes($id))
-        {
-            throw new Exception('No events found');
-        }
-
-        foreach ($this->getAllEventsWithCodes($id) as $event)
-        {
-            $clinicalEvent = new EventSpecialtyCode();
-
-            $content = ($event->EVENT_DETAIL != '') ? $event->EVENT_DETAIL : 'Unknown';
-
-            if ($clinicalEvent->checkEventIsNotExcluded($event->CLINICAL_SPECIALTY))
-            {
-                $dateTime = explode(' ', $event->EVENT_DATE);
-                $dateArray = explode('-', $dateTime[0]);
-                $timeArray = explode(':', $dateTime[1]);
-
-                $patientData[$counter] = array(
-                    'content' => $content,
-                    'start' => array(
-                        'year' => $dateArray[0],
-                        'month' => $dateArray[1],
-                        'day' => $dateArray[2],
-                        'hour' => $timeArray[0],
-                        'minute' => $timeArray[1],
-                        'second' => 0
-                    ),
-                    'group' => $event->CLINICAL_SPECIALTY,
-                    'cssClass' => str_replace(' ', '-', $event->CLINICAL_SPECIALTY),
-                    'type' => 'box',
-                    'id' => $event->UNIQUE_ID
-                );
-
-                $counter ++;
-            }
-        }
-
-        return $patientData;
-    }
-
-    /**
      * @param $patientId
      * @param $specialtyCodes
      * @return array
--- app/Patient.php
+++ app/Patient.php
@@ -13,10 +13,44 @@ class Patient extends Model
 
     public function getPatientEvents()
     {
-        $events = new Events();
+        $this->events = Event::where('BCI_ID', $this->BCI_ID)
+            ->orderBy('EVENT_DATE', 'asc')
+            ->get();
 
         try {
-            return $events->prepareEventDataForTemplate($this->BCI_ID);
+            $patientData = array();
+
+            foreach ($this->events as $event)
+            {
+                $clinicalEvent = new EventSpecialtyCode();
+
+                $content = ($event->EVENT_DETAIL != '') ? $event->EVENT_DETAIL : 'Unknown';
+
+                if ($clinicalEvent->checkEventIsNotExcluded($event->CLINICAL_SPECIALTY))
+                {
+                    $dateTime = explode(' ', $event->EVENT_DATE);
+                    $dateArray = explode('-', $dateTime[0]);
+                    $timeArray = explode(':', $dateTime[1]);
+
+                    $patientData[] = array(
+                        'content' => $content,
+                        'start' => array(
+                            'year' => $dateArray[0],
+                            'month' => $dateArray[1],
+                            'day' => $dateArray[2],
+                            'hour' => $timeArray[0],
+                            'minute' => $timeArray[1],
+                            'second' => 0
+                        ),
+                        'group' => $event->CLINICAL_SPECIALTY,
+                        'cssClass' => str_replace(' ', '-', $event->CLINICAL_SPECIALTY),
+                        'type' => 'box',
+                        'id' => $event->UNIQUE_ID
+                    );
+                }
+            }
+
+            return $patientData;
         }
         catch(Exception $e) {
             throw new Exception($e->getMessage());
